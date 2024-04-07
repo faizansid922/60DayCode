63eace334aa17e5699666eb9b503cde65a09083b
faizan sid <113455732+faizansid922@users.noreply.github.com>
2024-04-07 16:02:42 +0530

    Prime or Not
    Solve

---
+++ 2index.js
@@ -0,0 +1,15 @@
+function isPrime(num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Example usage
+console.log(isPrime(7)); // Output: true
+console.log(isPrime(15)); // Output: false
