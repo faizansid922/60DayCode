fd7b0c0c2a3b17bfc99bb6fc179c1a167c5d0cfa
faizan sid <113455732+faizansid922@users.noreply.github.com>
2024-04-07 16:11:49 +0530

    char is a small case or not.
    char is a small case or not.

---
+++ 5index.js
@@ -0,0 +1,8 @@
+function isLowerCase(char) {
+    return char >= 'a' && char <= 'z';
+}
+
+// Example usage
+console.log(isLowerCase('a')); // Output: true
+console.log(isLowerCase('A')); // Output: false
+console.log(isLowerCase('1')); // Output: false
